8c77b93a661ca20aa67a5f1bc1914e223317f7f4
Patrick Rogan <psr2125@columbia.edu>
2016-05-13 10:13:50 -0400

    minor commenting of code for text preprocessin in R

--- text_preprocessing.R
+++ text_preprocessing.R
@@ -8,25 +8,19 @@ require('SnowballC')
 require('RColorBrewer')
 require('ggplot2')
 require('tm')
-# source("https://bioconductor.org/biocLite.R")
-# biocLite("Rgraphviz")
 require('Rgraphviz')
 require('fpc')
 require('topicmodels')
 
-#setwd("C:\\Users\\HS\\Desktop\\school\\classes\\VIS\\HW_TEXT")
-
-mypwd = file.path('source')
+mypwd = file.path('data/source')
 
 docs = Corpus(DirSource(mypwd))
 
-#inspect(docs)
-
+# clean the text data by stripping out special characters, putting all words
+# in lower case, removing stop words, and stemming data
 toSpace = content_transformer(function(yourdata, target) gsub(target, ' ', yourdata))
 docs = tm_map(docs, toSpace, "/")
 docs = tm_map(docs, toSpace, "@")
-#docs = tm_map(docs, toSpace, "(")
-#docs = tm_map(docs, toSpace, ")")
 docs = tm_map(docs, toSpace, ";")
 docs = tm_map(docs, toSpace, "--")
 docs = tm_map(docs, toSpace, " +")
@@ -35,23 +29,22 @@ docs <- tm_map(docs, content_transformer(tolower))
 docs = tm_map(docs, removeNumbers)
 docs = tm_map(docs, removePunctuation)
 docs = tm_map(docs, removeWords, stopwords("english"))
-# docs = tm_map(docs, removeWords,c('school','department'))
 docs = tm_map(docs,stemDocument)
 docs = tm_map(docs,stripWhitespace)
 docs = tm_map(docs,PlainTextDocument)
 
+# Remove empty documents
 for(j in seq(docs))   
 {   
   docs[[j]]$content <- docs[[j]]$content[which(docs[[j]]$content != "")]
 }   
 
+# Create and save Document Term Matrix, Term Document Matrix, and save off
+# documents
 dtm = DocumentTermMatrix(docs)
 tdm = TermDocumentMatrix(docs)
 
-save(dtm, file="dtm.Rda")
-save(tdm, file="tdm.Rda")
-save(docs, file = "docs.Rda")
-
-#load("dtm.Rda")
-#load("tdm.Rda")
+save(dtm, file="data/dtm.Rda")
+save(tdm, file="data/tdm.Rda")
+save(docs, file = "data/docs.Rda")
 
